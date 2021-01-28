860f0fc331a2649dbe9f426ce2bd11e3ba458fd4
30nt <zontovv@outlook.com>
2021-01-28 21:23:12 +0200

    add homework

---
+++ lesson6.py
@@ -0,0 +1,34 @@
+#
+"""
+6)
+Даны списки my_list_1 и my_list_2.
+Создать список my_result в который поместить элементы из my_list_1 и
+my_list_2 через один, начиная с my_list_1.
+a) кол-во эл-тов одинаковое
+б) кол-во эл-тов разное. Оставшиеся дописать в конец.
+"""
+# my_list_2 = [1,2,3]
+# my_list_1 = [10,20,30,40,50, 60]
+# my_result = []
+#
+# if len(my_list_1) > len(my_list_2):  # Придумать как реализовать правильный порядок в my_result
+#     my_list_1, my_list_2 = my_list_2, my_list_1
+#
+# for index in range(len(my_list_1)):
+#     my_result.append(my_list_1[index])
+#     my_result.append(my_list_2[index])
+# my_result.extend(my_list_2[len(my_list_1):])
+
+# last_common_index = len(my_list_1) - 1
+# my_result.extend(my_list_2[last_common_index + 1:])
+
+# print(my_result)
+
+
+"""
+Цикл с условием(while)
+Игра с угадыванием числа.
+
+"""
+
+# split, join
